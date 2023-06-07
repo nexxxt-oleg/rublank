02697337c12de3ce77267a31fba09d7d0b31c774
Олег <nexxxt.07@gmail.com>
2023-06-07 14:07:58 +0300

    4

--- bitrix/templates/market_column/js/script5733.js
+++ bitrix/templates/market_column/js/script5733.js
@@ -217,4 +217,31 @@
 
 $(document).ready(function () {
    $('.fancyimg').fancybox();
+    var top_show = 500;
+    var delay = 1000;
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > top_show) $('.bxr-button-up').fadeIn();
+        else $('.bxr-button-up').fadeOut();
+    });
+
+    $('.bxr-button-up').click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, delay);
+    });
+
+
+    $('.bxr-slider').slick({
+        slidesToShow: 1,
+        dots: true,
+        fade: true,
+        speed: 1500,
+        autoplaySpeed: 3000,
+        autoplay: true,
+        nextArrow: '<button type="button" class="slick-next bxr-bg-hover-dark-flat"><i class="fa fa-angle-right"></i></button>',
+        prevArrow: '<button type="button" class="slick-prev bxr-bg-hover-dark-flat"><i class="fa fa-angle-left"></i></button>',
+    });
+
+    $('.bxr-slider').css("visibility", "visible");
 });
